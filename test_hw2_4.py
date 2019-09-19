bfdb6253ccd20017f5651943c7002548cc9f9139
Kira Bradley <kirabradley91@gmail.com>
2019-09-19 18:53:00 -0400

    my brach

--- test_hw2_4.py
+++ test_hw2_4.py
@@ -34,7 +34,7 @@ def test_title(env_setup):
     driver.find_element_by_id('nav-open-btn').click()
     driver.find_element_by_partial_link_text("https://www.kingsford.com/country/").click()
     driver.find_element_by_class_name('btn-outline white').send_keys(Keys.ENTER)
-    blah
+    blah blah blah
 
 
 
